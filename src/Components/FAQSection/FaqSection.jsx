bb528948a6cc4d3bfe80e4cc4102ba39f5df0e5b
nabilsiddik <nabilsiddik90@gmail.com>
2024-11-15 01:29:53 -0800

    faq done next day

--- src/Components/FAQSection/FaqSection.jsx
+++ src/Components/FAQSection/FaqSection.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './faq.css'
 import { FaAngleDown } from "react-icons/fa";
+import Accordion from '../Accordion/Accordion';
+import { FaArrowRight } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa";
+import { IoChatbubbleEllipses } from "react-icons/io5";
 
 const FaqSection = () => {
+
+    const [faq, setFaq] = useState([])
+
+    useState(()=>{
+        fetch('https://win24-assignment.azurewebsites.net/api/faq')
+        .then(res => res.json())
+        .then(data => setFaq(data))
+    }, [])
+
+
   return (
     <section id="faq">
             <div className="container flex justify-between gap-30">
@@ -26,98 +40,36 @@ const FaqSection = () => {
                 <div className="contact flex gap-20">
                 <div className="contact_box box_1 flex direction-column gap-10">
                     <span className="icon">
-                    <i className="fa-solid fa-phone" />
+                     <FaPhone />
                     </span>
                     <p>Still have questions?</p>
-                    <a href="#">
-                    contact us
+                    <a href="#" className='flex items-center gap-5'>
+                    <span> contact us</span>
                     <span className="icon">
-                        <i className="fa-solid fa-arrow-right" />
+                        <FaArrowRight />
                     </span>
                     </a>
                 </div>
                 <div className="contact_box box_2 flex direction-column gap-10">
                     <span className="icon">
-                    <i className="fa-brands fa-rocketchat" />
+                        <IoChatbubbleEllipses />
                     </span>
                     <p>Still have questions?</p>
-                    <a href="#">
-                    contact us
+                    <a href="#" className='flex items-center gap-5'>
+                    <span>contact us</span>
                     <span className="icon">
-                        <i className="fa-solid fa-arrow-right" />
+                        <FaArrowRight />
                     </span>
                     </a>
                 </div>
-                <button>Contact Us Now</button>
                 </div>
             </div>
             <div className="faq_right">
                 <div className="accordion">
                 <ul className="flex direction-column gap-10">
-                    <li className="flex items-center justify-between">
-                    <p>Is any of my personal information stored in the App?</p>
-                    <div className="icon">
-                        <span>
-                        <i className="fa-solid fa-angle-down" />
-                        </span>
-                    </div>
-                    </li>
-                    <li className="flex items-center justify-between">
-                    <p>What formats can I download my transaction history in?</p>
-                    <div className="icon">
-                        <span>
-                        <i className="fa-solid fa-angle-down" />
-                        </span>
-                    </div>
-                    </li>
-                    <li className="open">
-                    <div className="accordion_header flex items-center justify-between">
-                        <p>Can I schedule future transfers?</p>
-                        <div className="icon">
-                        <span>
-                            <i className="fa-solid fa-angle-up" />
-                        </span>
-                        </div>
-                    </div>
-                    <div className="accordion_content pt-20">
-                        <p>
-                        Nunc duis id aenean gravida tincidunt eu, tempor ullamcorper.
-                        Viverra aliquam arcu, viverra et, cursus. Aliquet pretium
-                        cursus adipiscing gravida et consequat lobortis arcu velit.
-                        Nibh pharetra fermentum duis accumsan lectus non. Massa cursus
-                        molestie lorem scelerisque pellentesque. Nisi, enim, arcu
-                        purus gravida adipiscing euismod montes, duis egestas.
-                        Vehicula eu etiam quam tristique tincidunt suspendisse ut
-                        consequat.
-                        </p>
-                    </div>
-                    </li>
-                    <li className="flex items-center justify-between">
-                    <p>When can I use Banking App services?</p>
-                    <div className="icon">
-                        <span>
-                        <i className="fa-solid fa-angle-down" />
-                        </span>
-                    </div>
-                    </li>
-                    <li className="flex items-center justify-between">
-                    <p>
-                        Can I create my own password that is easy for me to remember?
-                    </p>
-                    <div className="icon">
-                        <span>
-                        <i className="fa-solid fa-angle-down" />
-                        </span>
-                    </div>
-                    </li>
-                    <li className="flex items-center justify-between">
-                    <p>What happens if I forget or lose my password?</p>
-                    <div className="icon">
-                        <span>
-                        <i className="fa-solid fa-angle-down" />
-                        </span>
-                    </div>
-                    </li>
+                    {faq.length > 0 && faq.map((faqItem) => {
+                        return <Accordion key={faqItem.id} faq = {faqItem}/>
+                    })}
                 </ul>
                 </div>
             </div>
--- src/Components/TestimonialSection/TestimonialSection.jsx
+++ src/Components/TestimonialSection/TestimonialSection.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './testimonial.css'
-import quotesIcon from '../../assets/quotes.png'
-import { FaStar } from "react-icons/fa";
-import man from '../../assets/man.png'
-import women from '../../assets/woment.png'
+import Testimonial from './../Testimonial/Testimonial';
 
 const TestimonialSection = () => {
+
+  const [testimonial, setTestimonial] = useState([])
+
+  useEffect(()=>{
+    fetch('https://win24-assignment.azurewebsites.net/api/testimonials')
+    .then(res => res.json())
+    .then(data => setTestimonial(data))
+  }, [])
+
   return (
     <section id="testimonial">
     <div className="container flex justify-between gap-30">
@@ -15,80 +21,9 @@ const TestimonialSection = () => {
         </h2>
       </div>
       <div className="testimonials flex justify-between gap-30">
-        <div className="testimonial flex direction-column gap-10">
-          <span className="quote_icon">
-            <img src={quotesIcon} alt="" />
-          </span>
-          <ul className="rating flex items-center gap-5">
-            <li>
-                <FaStar />
-            </li>
-            <li>
-                <FaStar />
-            </li>
-            <li>
-                <FaStar />
-            </li>
-            <li>
-                <FaStar />
-            </li>
-            <li>
-                <FaStar />
-            </li>
-          </ul>
-          <p>
-            Sit pretium aliquam tempor, orci dolor sed maecenas rutrum
-            sagittis. Laoreet posuere rhoncus, egestas lacus, egestas justo
-            aliquam vel. Nisi vitae lectus hac hendrerit. Montes justo turpis
-            sit amet.
-          </p>
-          <div className="client_profil flex items-center gap-10">
-            <div className="profil_pic">
-              <img src={women} alt="" />
-            </div>
-            <div>
-              <h3>Fannie Summers</h3>
-              <p>Designer</p>
-            </div>
-          </div>
-        </div>
-        <div className="testimonial flex direction-column gap-10">
-          <span className="quote_icon">
-            <img src={quotesIcon} alt="" />
-          </span>
-          <ul className="rating flex items-center gap-5">
-            <li>
-                <FaStar />
-            </li>
-            <li>
-                <FaStar />
-            </li>
-            <li>
-                <FaStar />
-            </li>
-            <li>
-                <FaStar />
-            </li>
-            <li>
-                <FaStar />
-            </li>
-          </ul>
-          <p>
-            Sit pretium aliquam tempor, orci dolor sed maecenas rutrum
-            sagittis. Laoreet posuere rhoncus, egestas lacus, egestas justo
-            aliquam vel. Nisi vitae lectus hac hendrerit. Montes justo turpis
-            sit amet.
-          </p>
-          <div className="client_profil flex items-center gap-10">
-            <div className="profil_pic">
-              <img src={man} alt="" />
-            </div>
-            <div>
-              <h3>Fannie Summers</h3>
-              <p>Designer</p>
-            </div>
-          </div>
-        </div>
+          {testimonial.length > 0 && testimonial.map((testimonialItem) => {
+            return <Testimonial key={testimonialItem.id} testimonial={testimonialItem}/>
+          })}
       </div>
     </div>
   </section>
